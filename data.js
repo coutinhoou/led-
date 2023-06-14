8f8a4910ceb25acb2a7da887d0e7a50ea5311aac
coutinhoou <111984834+coutinhoou@users.noreply.github.com>
2023-06-14 11:52:07 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,11 @@
+function mudaRED(){
+    document.getElementById('myImage').src='led_red.png'
+}
+
+function mudaGRE(){
+    document.getElementById('myImage').src='led_gre.png'
+}
+
+function mudaIMG(img){
+    document.getElementById('myImage').src=img;'led_red.png'
+}
